0b85fb7cfaace8f28c048a8dcee37c572b00e9a5
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-16 18:14:45 -0300

    Executando o projeto com o método run do springboot

--- src/main/java/dio/springboot/PrimeirosPassosApplication.java
+++ src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -1,7 +1,10 @@
 package dio.springboot;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class PrimeirosPassosApplication {
@@ -9,5 +12,4 @@ public class PrimeirosPassosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeirosPassosApplication.class, args);
 	}
-
 }
